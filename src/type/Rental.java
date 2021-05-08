c7e697ef4ea6559730ea8f3672a7c2586bac1209
Slébatman <antoine.baetsle@gmail.com>
2021-05-08 18:03:14 +0200

    Create Class Object Principal

---
+++ src/type/Rental.java
@@ -0,0 +1,15 @@
+package type;
+
+import java.util.Collections;
+import java.util.GregorianCalendar;
+
+public class Rental {
+    // Link to DB
+    private int idRental;
+    private GregorianCalendar startDate;
+    private Object object;
+    private Collections tenant;
+    private GregorianCalendar returnDate;
+    private String rentalManager;
+
+}
