6f32288273cdd46047997817537e5c8568ce1b9b
Valentin <valentin.antoine@student.unamur.be>
2021-05-09 09:44:01 +0200

    modification fenetres

--- src/view/AddObjectWindow.java
+++ src/view/AddObjectWindow.java
@@ -1,13 +1,43 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AddObjectWindow extends JFrame {
+    private JLabel labelCollective, labelName, labelCommandable, labelDate, labelPrice, labelDeposit, labelPeriod;
+    private JComboBox listCollective;
+    private JPanel panel;
 
     public AddObjectWindow(){
         super("Create an object");
-        setBounds(150, 150, 400, 400);
+        setBounds(250, 200, 600, 400);
+        String[] collectives = {"info", "math", "philo", "éco" ,"chygé"};
+        listCollective = new JComboBox(collectives);
+        listCollective.setSelectedItem("info");
+        this.add(listCollective);
 
+        this.setLayout(new FlowLayout());
+
+
+        labelCollective = new JLabel("Collectif : ");
+        labelName = new JLabel("Nom de l'objet : ");
+        labelCommandable = new JLabel("Commandable : ");
+        labelDate = new JLabel("Date de l'achat : ");
+        labelPrice = new JLabel("Prix d'achat : ");
+        labelDeposit = new JLabel("Montant de la caution : ");
+        labelPeriod = new JLabel("Combien de jours maximum peut-il être loué :");
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(7,2));
+        panel.add(labelCollective);
+        panel.add(listCollective);
+        panel.add(labelName);
+        panel.add(labelCommandable);
+        panel.add(labelDate);
+        panel.add(labelPrice);
+        panel.add(labelDeposit);
+        panel.add(labelPeriod);
+        panel.setVisible(true);
+        this.add(panel);
         setVisible(true);
     }
 }
