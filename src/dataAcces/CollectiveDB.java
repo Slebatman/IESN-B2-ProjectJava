b990ec320166b0fa19808257844065b8c86844ad
Slébatman <antoine.baetsle@gmail.com>
2021-05-11 11:02:08 +0200

    Insert : PreparedStatement

--- src/dataAcces/CollectiveDB.java
+++ src/dataAcces/CollectiveDB.java
@@ -7,18 +7,18 @@ import java.sql.*;
 
 public class CollectiveDB implements ICollectiveDAO {
     // DB Acces
-    private SingletonConnexion connexion = new SingletonConnexion();
+    private Connection connexion = SingletonConnexion.getConnection();
 
     @Override
     public void insert(Collective c) throws DAOConfigurationException {
         try {
-            String sql = "INSERT INTO collective (name, category, physicalAdress, emailAdress) VALUES (" +
-                    c.getName() +", " +
-                    c.getCategory() + ", " +
-                    c.getPhysicalAdress() + ", " +
-                    c.getEmailAdress() +")";
-            Statement statement = this.connexion.getConnection().createStatement();
-            statement.executeUpdate(sql);
+            String sql = "INSERT INTO collective (name, category, physicalAdress, emailAdress) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connexion.prepareStatement(sql);
+            statement.setString(1, c.getName());
+            statement.setString(2,c.getCategory());
+            statement.setString(3,c.getPhysicalAdress());
+            statement.setString(4,c.getEmailAdress());
+            statement.executeUpdate();
         } catch (SQLException e) {
             throw new DAOConfigurationException("Une erreur d'accès à la base de données s'est produit, méthode appelée sur une connexion fermée ou erreur SQL.");
         }
