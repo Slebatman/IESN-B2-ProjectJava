9d7736ca7b208acea0a77f976cb0f0e1088e1a49
Slébatman <antoine.baetsle@gmail.com>
2021-05-17 17:58:59 +0200

    Cleancode TypeOfProblemRentalDAO

--- src/dataAcces/dao/ITypeOfProblemRentalDAO.java
+++ src/dataAcces/dao/ITypeOfProblemRentalDAO.java
@@ -5,6 +5,10 @@ import exception.DAOException;
 import java.util.ArrayList;
 
 public interface ITypeOfProblemRentalDAO {
+
+    // Select id of a problem base on its name
     int selectIdBaseName(String nameProblem) throws DAOException;
+
+    // Select DISTINCT name of typeOfProblemExitLocation
     ArrayList<String> selectDistinctNameOfTypeOfProblemExitRental() throws DAOException;
 }
