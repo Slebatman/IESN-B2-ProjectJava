b652fbbbcc3d4fed9186ded40fc046e28f99b033
Slébatman <antoine.baetsle@gmail.com>
2021-05-18 09:14:46 +0200

    Refactor package Controller

--- src/controller/ResearchController.java
+++ src/controller/ResearchController.java
@@ -1,4 +1,4 @@
-package controler;
+package controller;
 
 import model.FirstResearch;
 import model.SecondResearch;
@@ -9,21 +9,31 @@ import exception.DAOException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
-public class SearchControler {
+public class ResearchController {
+    // Variable
     private ResearchManager manager;
 
-    public SearchControler(){
-        manager = new ResearchManager();
+    // Constructor
+    public ResearchController(){
+        setManager(new ResearchManager());
     }
 
+    // Setter
+    private void setManager(ResearchManager manager) {
+        this.manager = manager;
+    }
+
+    // (Research n°1) All rentals for a category of collectives
     public ArrayList<FirstResearch> rentalsForOneCollectiveCategory(String category) throws DAOException {
         return manager.rentalsForOneCollectiveCategory(category);
     }
 
+    // (Research n°2) Select all ProblemExitRental based on a TypeOfExitRental
     public ArrayList<SecondResearch> problemRentalBasedOnTypeOfProblem(int idTypeOfProblemRental) throws DAOException {
         return manager.problemRentalBasedOnTypeOfProblem(idTypeOfProblemRental);
     }
 
+    // (Research n°3) All rentals between 2 dates
     public ArrayList<ThirdResearch> getRentalBetween2Dates(GregorianCalendar firstDate, GregorianCalendar secondDate) throws DAOException {
         return manager.getRentalBetween2Dates(firstDate, secondDate);
     }
