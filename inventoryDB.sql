be9854561cee38112c912622a515c3af3179407a
Slébatman <antoine.baetsle@gmail.com>
2021-05-20 09:22:04 +0200

    Update script createDB

--- inventoryDB.sql
+++ inventoryDB.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
---
--- Host: localhost    Database: inventory
--- ------------------------------------------------------
--- Server version	8.0.24
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -15,155 +9,95 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `collective`
---
+-- collective: table
+CREATE TABLE `collective`
+(
+    `idCollective`    int         NOT NULL AUTO_INCREMENT,
+    `name`            varchar(45) NOT NULL,
+    `category`        varchar(45) NOT NULL,
+    `physicalAddress` varchar(45) NOT NULL,
+    `emailAddress`    varchar(45) NOT NULL,
+    PRIMARY KEY (`idCollective`, `name`),
+    UNIQUE KEY `idCollective_UNIQUE` (`idCollective`),
+    UNIQUE KEY `name` (`name`),
+    CONSTRAINT `collective_chk_1` CHECK ((not ((`name` like _utf8mb3'')))),
+    CONSTRAINT `collective_chk_2` CHECK ((not ((`category` like _utf8mb3'')))),
+    CONSTRAINT `collective_chk_3` CHECK ((not ((`physicalAddress` like _utf8mb3'')))),
+    CONSTRAINT `collective_chk_4` CHECK ((not ((`emailAddress` like _utf8mb3''))))
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `collective`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `collective` (
-  `idCollective` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `category` varchar(45) NOT NULL,
-  `physicalAdress` varchar(45) NOT NULL,
-  `emailAdress` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCollective`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- oneobject: table
+CREATE TABLE `oneobject`
+(
+    `idObject`          int         NOT NULL AUTO_INCREMENT,
+    `name`              varchar(45) NOT NULL,
+    `idCollectiveOwner` int         NOT NULL,
+    `isCommendable`     tinyint     NOT NULL,
+    `purchaseDate`      date   DEFAULT NULL,
+    `purchasePrice`     double DEFAULT NULL,
+    `deposit`           int    DEFAULT 0,
+    `maxRentalPeriod`   int         NOT NULL,
+    PRIMARY KEY (`idObject`, `name`, `idCollectiveOwner`),
+    UNIQUE KEY `name` (`name`),
+    KEY `idCollectiveOwner_idx` (`idCollectiveOwner`),
+    CONSTRAINT `idCollectiveOwner` FOREIGN KEY (`idCollectiveOwner`) REFERENCES `collective` (`idCollective`),
+    CONSTRAINT `oneobject_chk_1` CHECK ((`purchasePrice` > 0)),
+    CONSTRAINT `oneobject_chk_2` CHECK ((`deposit` >= 0)),
+    CONSTRAINT `oneobject_chk_3` CHECK ((`maxRentalPeriod` > 0)),
+    CONSTRAINT `oneobject_chk_4` CHECK ((not ((`name` like _utf8mb3''))))
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
 
---
--- Dumping data for table `collective`
---
+-- problemexitrental: table
+CREATE TABLE `problemexitrental`
+(
+    `idproblemExitLocation` int    NOT NULL AUTO_INCREMENT,
+    `idRental`              int    NOT NULL,
+    `idTypeOfProblemRental` int    NOT NULL,
+    `invocedPrice`          double NOT NULL,
+    `note`                  longtext,
+    PRIMARY KEY (`idproblemExitLocation`, `idRental`),
+    UNIQUE KEY `idRental` (`idRental`),
+    KEY `idRental_idx` (`idRental`),
+    KEY `idTypeOfProblemRental_idx` (`idTypeOfProblemRental`),
+    CONSTRAINT `idRental` FOREIGN KEY (`idRental`) REFERENCES `rental` (`idRental`),
+    CONSTRAINT `idTypeOfProblemRental` FOREIGN KEY (`idTypeOfProblemRental`) REFERENCES `typeofproblemrental` (`idtypeofproblemrental`),
+    CONSTRAINT `problemexitrental_chk_1` CHECK ((`invocedPrice` >= 0)),
+    CONSTRAINT `problemexitrental_chk_2` CHECK ((not ((`note` like _utf8mb3''))))
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
 
-LOCK TABLES `collective` WRITE;
-/*!40000 ALTER TABLE `collective` DISABLE KEYS */;
-/*!40000 ALTER TABLE `collective` ENABLE KEYS */;
-UNLOCK TABLES;
+-- rental: table
+CREATE TABLE `rental`
+(
+    `idRental`      int         NOT NULL AUTO_INCREMENT,
+    `startDate`     date        NOT NULL,
+    `idObject`      int         NOT NULL,
+    `idTenant`      int         NOT NULL,
+    `returnDate`    date DEFAULT NULL,
+    `rentalManager` varchar(45) NOT NULL,
+    PRIMARY KEY (`idRental`, `startDate`, `idObject`),
+    UNIQUE KEY `startDate` (`startDate`, `idObject`),
+    KEY `idObject_idx` (`idObject`),
+    KEY `idTenant_idx` (`idTenant`),
+    CONSTRAINT `idObject` FOREIGN KEY (`idObject`) REFERENCES `oneobject` (`idObject`),
+    CONSTRAINT `idTenant` FOREIGN KEY (`idTenant`) REFERENCES `collective` (`idCollective`),
+    CONSTRAINT `rental_chk_1` CHECK ((not ((`rentalManager` like _utf8mb3'')))),
+    CONSTRAINT `rental_chk_2` CHECK ((`returnDate` >= `startDate`))
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
 
---
--- Table structure for table `object`
---
+-- typeofproblemrental: table
+CREATE TABLE `typeofproblemrental`
+(
+    `idtypeofproblemrental` int         NOT NULL AUTO_INCREMENT,
+    `name`                  varchar(45) NOT NULL,
+    `description`           mediumtext  NOT NULL,
+    PRIMARY KEY (`idtypeofproblemrental`, `name`),
+    UNIQUE KEY `name` (`name`),
+    CONSTRAINT `typeofproblemrental_chk_1` CHECK ((not ((`name` like _utf8mb3'')))),
+    CONSTRAINT `typeofproblemrental_chk_2` CHECK ((not ((`description` like _utf8mb3''))))
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3;
 
-DROP TABLE IF EXISTS `object`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `object` (
-  `idObject` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `idCollectiveOwner` int NOT NULL,
-  `isCommandable` tinyint NOT NULL,
-  `purchaseDate` date DEFAULT NULL,
-  `purchasePrice` double DEFAULT NULL,
-  `deposit` int DEFAULT NULL,
-  `maxRentalPeriod` int NOT NULL,
-  PRIMARY KEY (`idObject`,`name`,`idCollectiveOwner`),
-  KEY `idCollectiveOwner_idx` (`idCollectiveOwner`),
-  CONSTRAINT `idCollectiveOwner` FOREIGN KEY (`idCollectiveOwner`) REFERENCES `collective` (`idCollective`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `object`
---
-
-LOCK TABLES `object` WRITE;
-/*!40000 ALTER TABLE `object` DISABLE KEYS */;
-/*!40000 ALTER TABLE `object` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `problemexitlocation`
---
-
-DROP TABLE IF EXISTS `problemexitlocation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `problemexitlocation` (
-  `idproblemExitLocation` int NOT NULL,
-  `idRental` int NOT NULL,
-  `idTypeOfProblemRental` int NOT NULL,
-  `invocedPrice` double NOT NULL,
-  `note` longtext,
-  PRIMARY KEY (`idproblemExitLocation`,`idRental`),
-  KEY `idRental_idx` (`idRental`),
-  KEY `idTypeOfProblemRental_idx` (`idTypeOfProblemRental`),
-  CONSTRAINT `idRental` FOREIGN KEY (`idRental`) REFERENCES `rental` (`idRental`),
-  CONSTRAINT `idTypeOfProblemRental` FOREIGN KEY (`idTypeOfProblemRental`) REFERENCES `typeofproblemrental` (`idtypeofproblemrental`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `problemexitlocation`
---
-
-LOCK TABLES `problemexitlocation` WRITE;
-/*!40000 ALTER TABLE `problemexitlocation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `problemexitlocation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `rental`
---
-
-DROP TABLE IF EXISTS `rental`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `rental` (
-  `idRental` int NOT NULL,
-  `startDate` date NOT NULL,
-  `idObject` int NOT NULL,
-  `idTenant` int NOT NULL,
-  `returnDate` date DEFAULT NULL,
-  `rentalManager` varchar(45) NOT NULL,
-  PRIMARY KEY (`idRental`,`startDate`,`idObject`),
-  KEY `idObject_idx` (`idObject`),
-  KEY `idTenant_idx` (`idTenant`),
-  CONSTRAINT `idObject` FOREIGN KEY (`idObject`) REFERENCES `object` (`idObject`),
-  CONSTRAINT `idTenant` FOREIGN KEY (`idTenant`) REFERENCES `collective` (`idCollective`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rental`
---
-
-LOCK TABLES `rental` WRITE;
-/*!40000 ALTER TABLE `rental` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rental` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `typeofproblemrental`
---
-
-DROP TABLE IF EXISTS `typeofproblemrental`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `typeofproblemrental` (
-  `idtypeofproblemrental` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `description` mediumtext NOT NULL,
-  PRIMARY KEY (`idtypeofproblemrental`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `typeofproblemrental`
---
-
-LOCK TABLES `typeofproblemrental` WRITE;
-/*!40000 ALTER TABLE `typeofproblemrental` DISABLE KEYS */;
-/*!40000 ALTER TABLE `typeofproblemrental` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-05-04 10:41:13
